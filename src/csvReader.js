7b3a67d32f22363ee0b1ac71d19bc07b7bc72b24
Sumsum1231 <sumitdash1971@gmail.com>
2024-04-05 10:31:16 +0530

    feat: step 20

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,22 +1,71 @@
-// src/csvReader.js
+const fs = require("fs");
+const csv = require("csv-parser");
 
-const fs = require('fs');
-const csv = require('csv-parser');
+// Helper function to convert data
+function convertData(data) {
+  let writeContent = "";
+  let rowsInserted = 0;
 
-function readCSV(filePath) {
-    const results = [];
+  data.forEach((row) => {
+    for (const key in row) {
+      writeContent += `${row[key]},`;
+    }
+    writeContent = writeContent.slice(0, -1);
+    writeContent += "\n";
+    rowsInserted += 1;
+  });
 
-    return new Promise((resolve, reject) => {
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', () => {
-                resolve(results);
-            })
-            .on('error', (error) => {
-                reject(error);
-            });
-    });
+  return { writeContent, rowsInserted };
 }
 
-module.exports = readCSV;
+function readCSV(filePath) {
+  const results = [];
+
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => {
+        results.push(data);
+      })
+      .on("end", () => {
+        resolve(results);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+}
+
+async function writeCSV(filePath, data) {
+  const { writeContent, rowsInserted } = convertData(data);
+
+  let fields = "";
+
+  for (const key in data[0]) {
+    fields += `${key},`;
+  }
+  fields = fields.slice(0, -1);
+  fields += "\n" + writeContent;
+
+  fs.writeFile(filePath, fields, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+    }
+  });
+}
+
+async function updateCSV(filePath, data) {
+  const { writeContent, rowsInserted } = convertData(data);
+
+  fs.appendFile(filePath, writeContent, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+    }
+  });
+
+  return { rowsInserted };
+}
+
+module.exports = { readCSV, writeCSV, updateCSV };
